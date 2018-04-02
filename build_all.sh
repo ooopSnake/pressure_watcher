38efb06ec17b119b2b430def82a348005dda43cc
suzhen <suzhen@homeinpocket.net>
2018-04-02 09:26:24 +0800

    enhance build_all script

--- build_all.sh
+++ build_all.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
+
 echo "delete previous builds"
 rm -rf pressure_watcher_*
 
 buildPrefix="pressure_watcher_"
 
-echo "build for arm linux"
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go  build -v -o ${buildPrefix}arm
+echo "building for arm linux"
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -ldflags "-w -s" -v -o ${buildPrefix}arm1
 
-echo "build for x86 linux"
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -v -o ${buildPrefix}x86
+echo "building for x86 linux"
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-w -s" -v -o ${buildPrefix}x86
 
-echo "build for x64 linux"
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o ${buildPrefix}x64
+echo "building for x64 linux"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -v -o ${buildPrefix}x64
 
 echo "all done"
